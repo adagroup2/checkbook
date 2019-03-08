f130d197b4df69b26a77bd914024d271b3c3ccb4
Michael P. Moran <mmutexas@gmail.com>
2019-03-08 12:20:49 -0600

    add test for write_record()

--- test_checkbook.py
+++ test_checkbook.py
@@ -1,5 +1,7 @@
 import checkbook
+import csv
 import datetime
+import os
 
 
 def test_create_withdraw_record():
@@ -18,5 +20,23 @@ def test_view_balance():
     assert checkbook.view_balance("dummy_ledger_file3.csv") == -20.00
 
 
-# def test_withdraw():
-#     assert withdraw
+def test_write_record():
+    dummy_filename = "write_record_dummy.csv"
+    checkbook.write_record(
+        dummy_filename, checkbook.create_withdraw_record(20.50)
+    )
+    checkbook.write_record(
+        dummy_filename, checkbook.create_withdraw_record(60.33)
+    )
+    checkbook.write_record(
+        dummy_filename, checkbook.create_withdraw_record(1.98)
+    )
+
+    with open(dummy_filename) as df:
+        reader = csv.DictReader(df, checkbook.FIELDNAMES)
+        amounts = [row[checkbook.AMOUNT_ROW] for row in reader]
+        assert float(amounts[0]) == -20.50
+        assert float(amounts[1]) == -60.33
+        assert float(amounts[2]) == -1.98
+
+    os.remove(dummy_filename)
