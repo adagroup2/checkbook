9dcd706c8418b6bee3814dc62741eb199235f2c8
Michael P. Moran <mmutexas@gmail.com>
2019-03-09 14:43:48 -0600

    refactor checkbook_loop(); add options constants; reformat code; ensure two decimal places for amount in create_deposit_record() and create_withdraw_record(); fix bug in last_row_id(); refactor is_valid_amount(); add file_exists() and create_ledger_file(); add is_valid_action_choice() and get_action_choice()

--- checkbook.py
+++ checkbook.py
@@ -18,29 +18,45 @@ CSV_HEADER = {
 }
 
 LEDGER_FILENAME = "ledger.csv"
+
+OPTION_VIEW_BALANCE = "1"
+OPTION_WITHDRAW = "2"
+OPTION_DEPOSIT = "3"
+OPTION_VIEW_HISTORY = "4"
+OPTION_EXIT = "5"
+
+OPTIONS = (
+    OPTION_VIEW_BALANCE,
+    OPTION_WITHDRAW,
+    OPTION_DEPOSIT,
+    OPTION_VIEW_HISTORY,
+    OPTION_EXIT,
+)
 ##############################################################################
 
 
 def get_trans(ledger_file):
-    '''
+    """
     str -> list
     ledger_file is name of the ledger file
     composes a list of dictionaries from ledger
-    '''
+    """
     with open(ledger_file) as lf:
-        transact_list = [{k: v for k, v in row.items()}
-                         for row in csv.DictReader(lf, skipinitialspace=True)]
+        transact_list = [
+            {k: v for k, v in row.items()}
+            for row in csv.DictReader(lf, skipinitialspace=True)
+        ]
         return transact_list
 
 
 def print_all(ledg_list):
-    '''
+    """
     list -> str
     prints all transactions to console
-    '''
+    """
     for dict in ledg_list:
         for key in dict:
-            print('{}: {}'.format(key, dict[key]))
+            print("{}: {}".format(key, dict[key]))
 
 
 def view_balance(ledger_file):
@@ -86,7 +102,7 @@ def create_deposit_record(category, description, amount):
         TIMESTAMP_ROW: timestamp,
         CATEGORY_ROW: category,
         DESCRIPTION_ROW: description,
-        AMOUNT_ROW: amount,
+        AMOUNT_ROW: f"{amount:.2f}",
     }
 
 
@@ -105,7 +121,7 @@ def create_withdraw_record(category, description, amount):
         TIMESTAMP_ROW: timestamp,
         CATEGORY_ROW: category,
         DESCRIPTION_ROW: description,
-        AMOUNT_ROW: -1 * amount,
+        AMOUNT_ROW: f"{-1 * amount:.2f}",
     }
 
 
@@ -124,7 +140,7 @@ def last_row_id(ledger_file):
     with open(ledger_file) as lf:
         reader = csv.DictReader(lf, FIELDNAMES)
         rows = [row for row in reader]
-        return int(reader[-1][ID_ROW]) if len(rows) > 0 else 0
+        return int(rows[-1][ID_ROW]) if len(rows) > 1 else 0
 
 
 def is_valid_amount(amount):
@@ -155,96 +171,151 @@ def get_valid_amount(prompt):
     return the amount user inputs cast to a float
     """
     input_amount = input(prompt)
-    while not is_valid_amount(input_amount):
-        input_amount = input("Please enter a dollar value (e.g., $50.50): ")
-    return float(input_amount)
+    if is_valid_amount(input_amount):
+        return float(input_amount)
+    else:
+        print("\nPlease enter a dollar value (e.g., $50.50)\n")
+        return get_valid_amount(prompt)
 
 
 def get_date():
-    '''
+    """
      -> str
     prompts for string user input
 
     returns a date string formatted for searching through ledger dictionary
-    '''
-    year_val = input('Please enter a year in form XXXX: ')
+    """
+    year_val = input("Please enter a year in form XXXX: ")
     while not len(year_val) == 4 or not year_val.isdigit():
-        year_val = input('Please enter a year in form XXXX: ')
-    month_val = input('Please enter a month in form XX: ')
+        year_val = input("Please enter a year in form XXXX: ")
+    month_val = input("Please enter a month in form XX: ")
     while not len(month_val) == 2 or not month_val.isdigit():
-        month_val = input('Please enter a month in form XX: ')
-    day_val = input('Please enter a day in form XX: ')
+        month_val = input("Please enter a month in form XX: ")
+    day_val = input("Please enter a day in form XX: ")
     while not len(day_val) == 2 or not day_val.isdigit():
-        month_val = input('Please enter a day in form XX: ')
-    date_string = year_val + '-' + month_val + '-' + day_val
+        month_val = input("Please enter a day in form XX: ")
+    date_string = year_val + "-" + month_val + "-" + day_val
     return date_string
 
+
 def get_cat():
-    '''
+    """
 
     --> str
     prompts user for string input
     returns a string
 
-    '''
-    cat_value = input('Please enter a category: ')
+    """
+    cat_value = input("Please enter a category: ")
     return cat_value
 
 
 def print_by_date(some_date, ledg_list):
-    '''
+    """
     str-> str
 
     some_date is string inputted date
     returns  string transactions from dictionary where date matches parameters presented
-    '''
+    """
     for dict in ledg_list:
-        if dict['timestamp'].startswith(some_date):
+        if dict["timestamp"].startswith(some_date):
             for key in dict:
-                print('{}: {}'.format(key, dict[key]))
-            print('--------------------')
+                print("{}: {}".format(key, dict[key]))
+            print("--------------------")
+
 
 def print_by_cat(some_cat, ledg_list):
-    '''
+    """
     str-> str
 
     some_cat is string inputted category
     returns  string transactions from dictionary where category matches parameters presented
-    '''
+    """
     for dict in ledg_list:
-        if dict['category'] == some_cat:
+        if dict["category"] == some_cat:
             for key in dict:
-                print('{}: {}'.format(key, dict[key]))
-            print('--------------------')
+                print("{}: {}".format(key, dict[key]))
+            print("--------------------")
+
+
+def file_exists(ledger_filename):
+    """
+    str -> bool
+
+    ledger_filename is the name of the ledger file
+
+    return True if ledger file exists; otherwise, False
+    """
+    return os.path.isfile(ledger_filename)
+
+
+def create_ledger_file(ledger_filename):
+    """
+    str -> None
+
+    ledger_filename is the name of the ledger file
+
+    create ledger file
+    CAUTION: will overwrite the ledger file if it exists
+    """
+    with open(ledger_filename, "w") as lf:
+        writer = csv.DictWriter(lf, FIELDNAMES)
+        writer.writerow(CSV_HEADER)
+
+
+def is_valid_action_choice(action_choice):
+    """
+    str -> bool
+
+    action_choice is the user's inputted action choice
+
+    return True if user's action choice is valid; otherwise, False
+    """
+    return action_choice.isdigit() and action_choice in OPTIONS
+
+
+def get_action_choice(prompt):
+    """
+    str -> str
+
+    prompt is the prompt to present to the user
+
+    return user's action choice
+    """
+    action_choice = input(prompt)
+    if is_valid_action_choice(action_choice):
+        return action_choice
+    else:
+        print(
+            f"\nInvalid choice: {action_choice}\n"
+            f"Please enter {OPTION_VIEW_BALANCE}-{OPTION_EXIT}\n"
+        )
+        return get_action_choice(prompt)
 
 
 def checkbook_loop():
     """
     implements CLI for checkbook application
     """
-    curr_bal = view_balance
-    prompt = (
-        "What would you like to do?\n\n"
-        "1) View current balance\n"
-        "2) Record a debit (withdraw)\n"
-        "3) Record a credit (deposit)\n"
-        "4) View Transaction History\n"
-        "5) Exit\n\n"
+    menu = (
+        f"What would you like to do?\n\n"
+        f"{OPTION_VIEW_BALANCE}) View current balance\n"
+        f"{OPTION_WITHDRAW}) Record a debit (withdraw)\n"
+        f"{OPTION_DEPOSIT}) Record a credit (deposit)\n"
+        f"{OPTION_VIEW_HISTORY}) View transaction history\n"
+        f"{OPTION_EXIT}) Exit\n"
     )
-    action_choice = input(prompt)
-
-    while action_choice not in ("12345"):
-        action_choice = input(
-            f"Invalid choice: {action_choice}\n\nPlease enter 1-5: "
-        )
+    print(menu)
+    prompt = "Your choice? "
+    action_choice = get_action_choice(prompt)
 
     # process menu choice #####################################################
-    if int(action_choice) == 1:
-        curr_bal = view_balance(LEDGER_FILENAME)
-        print("\nYour current balance is : ${}".format(curr_bal))
+    if action_choice == OPTION_VIEW_BALANCE:
+        balance = view_balance(LEDGER_FILENAME)
+        print(f"\nYour current balance is : ${balance:,.2f}")
         print()
 
-    elif int(action_choice) == 2:
+    elif action_choice == OPTION_WITHDRAW:
         category = input("\nEnter a category for withdrawal: ")
         description = input("Enter a description for withdrawal: ")
 
@@ -256,7 +327,7 @@ def checkbook_loop():
         )
         write_record(LEDGER_FILENAME, withdraw_record)
 
-    elif int(action_choice) == 3:
+    elif action_choice == OPTION_DEPOSIT:
         category = input("\nEnter a category for deposit: ")
         description = input("Enter a description for deposit: ")
 
@@ -268,42 +339,44 @@ def checkbook_loop():
         )
         write_record(LEDGER_FILENAME, deposit_record)
 
-    elif int(action_choice) == 4:
+    elif action_choice == OPTION_VIEW_HISTORY:
         ledger_list = get_trans(LEDGER_FILENAME)
         print_all(ledger_list)
         history_choice = input(
-            '\nWould you like to search transactions? y/n: \n\n')
-        if history_choice.startswith('y'.lower()):
+            "\nWould you like to search transactions? y/n: \n\n"
+        )
+        if history_choice.startswith("y".lower()):
             search_choice = input(
-                '1) Select By Date\n2) Select By Category\n3) Select By Description\n4) Exit to main menu \nYour Choice? \n''')
+                "1) Select By Date\n2) Select By Category\n3) Select By Description\n4) Exit to main menu \nYour Choice? \n"
+                ""
+            )
             while search_choice not in ("1234"):
                 search_choice = input(
-                    f"Invalid choice: {action_choice}\n\nPlease enter 1-4: ")
+                    f"Invalid choice: {action_choice}\n\nPlease enter 1-4: "
+                )
             if int(search_choice) == 1:
-                print('1: Search by Date\n')
+                print("1: Search by Date\n")
                 day = get_date()
                 print_by_date(day, ledger_list)
             elif int(search_choice) == 2:
-                print('2: Search by Category\n')
+                print("2: Search by Category\n")
                 category = get_cat()
                 print_by_cat(category, ledger_list)
             elif int(search_choice) == 3:
-                print('3: Search by Description keyword\n')
+                print("3: Search by Description keyword\n")
             elif int(search_choice) == 4:
-                print('returning to main menu\n')
+                print("returning to main menu\n")
 
-    elif int(action_choice) == 5:
-        exit()
+    elif action_choice == OPTION_EXIT:
+        exit(0)
     ##########################################################################
 
     return checkbook_loop()
 
 
 if __name__ == "__main__":
-    if not os.path.isfile(LEDGER_FILENAME):
-        with open(LEDGER_FILENAME, "w") as lf:
-            writer = csv.DictWriter(lf, FIELDNAMES)
-            writer.writerow(CSV_HEADER)
+    if not file_exists(LEDGER_FILENAME):
+        create_ledger_file(LEDGER_FILENAME)
 
     print("\n~~~ Welcome to your terminal checkbook! ~~~\n")
     checkbook_loop()
